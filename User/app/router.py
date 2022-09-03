08b2f817259453150eb50e9a5c37061d980caa4f
jungmir <ham4051@gmail.com>
2022-09-04 01:07:33 +0900

    Modify response which didn't update user

--- User/app/router.py
+++ User/app/router.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Any, Dict
 
 from database import Mongo
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body, Response, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from models import CreateUser, UpdateUser
@@ -48,10 +48,7 @@ async def update_user(id: str, user: UpdateUser = Body()) -> JSONResponse:
     mongo = Mongo()
     with mongo("user") as user_database:
         matched, modified = user_database.update_by_id(id, user.dict())
-    response.update(result={"matched": matched, "modified": modified})
     if matched == 0:
-        response.update(error={"message": "Not exist matched item"})
-        return JSONResponse(
-            content=jsonable_encoder(response), status_code=status.HTTP_404_NOT_FOUND
-        )
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    response.update(result={"matched": matched, "modified": modified})
     return JSONResponse(content=jsonable_encoder(response))
