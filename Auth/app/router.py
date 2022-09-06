33489274fa7dafd262b7c0ed3f145f8101aff880
jungmir <ham4051@gmail.com>
2022-09-06 20:10:06 +0900

    Modify logic about generate token and verify token

--- Auth/app/router.py
+++ Auth/app/router.py
@@ -1,42 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from datetime import timedelta
 from typing import Any, Dict
 
 from conf import config
-from fastapi import APIRouter, Depends, Header, Response, status
+from fastapi import APIRouter, Body, Header, Response, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from httpx import AsyncClient
-from models import Token, UserData
+from models import SimpleUser, Token
 
 router = APIRouter()
 cfg = config("server")
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("", status_code=status.HTTP_201_CREATED)
 async def generate_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
+    user: SimpleUser = Body(),
 ) -> JSONResponse:
     response: Dict[str, Any] = {}
-    async with AsyncClient() as client:
-        resp = await client.get(cfg.user.url, params=dict(user_id=form_data.username))
-    if resp.status_code != status.HTTP_200_OK:
-        return Response(status_code=resp.status_code)
-    json = resp.json()
-    user = UserData(**json.get("result"))
-    if form_data.password != user.password:
-        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
     token = Token.generate(user)
-    print(token)
     response.update(result=token)
     return JSONResponse(content=jsonable_encoder(response))
 
 
-@router.head("/verify", status_code=status.HTTP_200_OK)
+@router.get("/verify", status_code=status.HTTP_200_OK)
 async def verify_token(authorization: str = Header()) -> Response:
-    if not Token.verify(authorization):
-        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+    token_type, access_token = authorization.split()
+    token = Token(type=token_type, access_token=access_token)
+    if not token.verify():
+        headers = dict(user=token.payload)
+        return Response(status_code=status.HTTP_401_UNAUTHORIZED, headers=headers)
     return Response()
