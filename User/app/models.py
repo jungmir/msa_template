a2cd7b2b45acdcc89d57386880f2cc97f0310a19
jungmir <ham4051@gmail.com>
2022-09-06 19:53:06 +0900

    Add User Model each situation

--- User/app/models.py
+++ User/app/models.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from typing import Optional, Union
+from typing import Union
 
 from bson.objectid import ObjectId
-from pydantic import BaseModel, validator
+from const import Role
+from pydantic import BaseModel, Field, validator
 
 
-class User(BaseModel):
+class LoginUser(BaseModel):
+    user_id: str
+
+
+class UserRole(BaseModel):
+    role: Union[Role, str]
+
+    @validator("role")
+    def role_validator(cls, value: Union[Role, str]) -> Role:
+        if isinstance(value, Role):
+            return value
+        if hasattr(Role, value.upper()):
+            return getattr(Role, value.upper())
+        raise ValueError(f"'{value}' is not support, use only 'admin' or 'user'")
+
+
+class User(UserRole, LoginUser):
     name: str
+    password: str
     age: int
+    role: Union[Role, str] = Role.USER
 
 
 class CreateUser(User):
@@ -20,8 +39,13 @@ class UpdateUser(User):
     pass
 
 
+class SimpleUser(BaseModel):
+    sub: str
+    role: str
+
+
 class UserResponse(User):
-    id: Union[ObjectId, str]
+    id: Union[ObjectId, str] = Field(alias="_id")
 
     @validator("id")
     def id_validator(cls, value: Union[ObjectId, str]) -> str:
@@ -32,5 +56,4 @@ class UserResponse(User):
         raise ValueError(f"Unexpected Type: {type(value)}, only use 'ObjectId' or 'str'")
 
     class Config:
-        fileds = {"id": "_id"}
         arbitrary_types_allowed = True
